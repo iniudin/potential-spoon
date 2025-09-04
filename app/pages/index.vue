0faac6d5a7d3d5bd98f6da7c59e664458e56776b
Ahmad Syaifudin <sysfdn@pm.me>
2025-09-04 10:19:06 +0700

    feat: init project

---
+++ app/pages/index.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+const { locale } = useI18n()
+
+</script>
+
+<template>
+  <div>
+    my locale is {{ locale }}
+  </div>
+</template>
+
---
+++ server/api/hello.ts
@@ -0,0 +1,4 @@
+export default defineEventHandler((event) => {
+  const ctx = event.context
+  return ctx
+})
