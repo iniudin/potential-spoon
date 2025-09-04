8af87c34eddf1159ed28efcedee83094021078ad
Ahmad Syaifudin <sysfdn@pm.me>
2025-09-04 10:21:38 +0700

    chore: nitro preset for cf

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxtjs/i18n']
+  modules: ['@nuxt/eslint', '@nuxtjs/i18n'],
+  nitro: {
+    preset: "cloudflare_module",
+    cloudflare: {
+      deployConfig: true,
+      nodeCompat: true
+    }
+  }
 })
