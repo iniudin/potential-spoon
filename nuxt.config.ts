666e9ee382e002fe7312059b81032560f1285fbe
Ahmad Syaifudin <sysfdn@pm.me>
2025-09-09 12:48:30 +0700

    add nuxt hub

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxtjs/i18n'],
+  modules: ['@nuxt/eslint', '@nuxtjs/i18n', '@nuxthub/core'],
   nitro: {
     preset: "cloudflare_module",
     cloudflare: {
